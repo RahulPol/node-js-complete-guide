51b19dab53ccc6bb73c176e21fd90f42999cdaef
RahulVPol <rahul.pol@cimpress.com>
2021-01-10 00:41:00 +0530

    section-5-tut-67 handled 404 not found route

--- routes/shop.js
+++ routes/shop.js
@@ -3,7 +3,7 @@ const express = require("express");
 const router = express.Router();
 
 // handle root url request
-router.use("/", (req, res, next) => {
+router.get("/", (req, res, next) => {
   res.send("<h1>Hello from Express!</h1>");
 });
 
