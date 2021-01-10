e965d84fdb92ceadce049181121b64caa5f1e360
RahulVPol <rahul.pol@cimpress.com>
2021-01-11 01:26:02 +0530

    section-5-tut-70 Serve html pages

--- routes/admin.js
+++ routes/admin.js
@@ -1,21 +1,10 @@
+const path = require("path");
 const express = require("express");
 
 const router = express.Router();
 
 router.get("/add-product", (req, res, next) => {
-  res.send(`
-      <html>
-        <head>
-          <title>Add Product</title>
-        </head>
-        <body>
-          <form action="/admin/product" method="POST">
-              Product: <input name="productName" type="text" />
-              <button type="submit">Add Product</button>
-          </form>
-        </body>
-      </html>
-    `);
+  res.sendFile(path.join(__dirname, "../", "views", "add-product.html"));
 });
 
 router.post("/product", (req, res) => {
