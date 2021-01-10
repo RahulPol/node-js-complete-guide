9df4a4f424bce8c213bdec9955a66a58f37b1ebe
RahulVPol <rahul.pol@cimpress.com>
2021-01-11 01:40:17 +0530

    section-5-tut-72 Using helper function for navigation

--- routes/shop.js
+++ routes/shop.js
@@ -1,11 +1,13 @@
 const path = require("path");
 const express = require("express");
 
+const rootDir = require("../util/util");
+
 const router = express.Router();
 
 // handle root url request
 router.get("/", (req, res, next) => {
-  res.sendFile(path.join(__dirname, "../", "views", "shop.html"));
+  res.sendFile(path.join(rootDir, "views", "shop.html"));
 });
 
 module.exports = router;
